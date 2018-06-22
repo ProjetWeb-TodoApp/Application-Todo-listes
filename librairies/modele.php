8260c408dbceab68348b25afe486eee6438d39f9
Unknown <nicolas10410@GMAIL.COM>
2018-06-22 14:29:35 +0200

    modele
    focntion de connexion

--- librairies/modele.php
+++ librairies/modele.php
@@ -1,4 +1,7 @@
 <?php
+include_once('maLibSQL.pdo.php');
+
+
 //renvoi rien
 //les id des utlisateurs sont rentrés en tableaux dans usr_tab
 function new_task($grp,$id_usr_tab,$label,$chef=null,$description=null,$deadline,$grp_id,$parent=null){}
@@ -8,15 +11,29 @@ function new_checklist($tsk_id,$etat,$label){}
 function edit_task($grp,$usr_tab,$label,$chef=null,$description=null,$deadline,$grp_id,$parent=null){}
 
 //field_tab correspond aux champs que l'on veut afficher en tableau
-function affiche_task_group($field_tab,$order_by=null){}
+function prompt_task_group($group,$order_by=null){
+    $sql='SELECT * from task where group=$group ';
+    SQLSelect($sql);
 
-function affiche_task_user($field_tab,$order_by=null){}
+
+}
+
+function prompt_task_user($user,$order_by=null){}
 
 
-function affiche_checklist_task(){}
+function prompt_checklist_task(){}
 
-function verif_user_BDD(){}
+function check_user_BDD($login,$password){
+    $sql="SELECT login, password from user where login='$login '";
+    $user=parcoursRS(SQLSelect($sql));
+    if ($user["password"]==$password){return $user["id"];}
+    else {return false;};
+}
 
+function is_project_manager($id){
+    $sql="SELECT project_manager from user where id=$id";
+    return(SQLGetChamp($sql)==1);
+}
 function late_task(){}
 
 function delete_task(){}
