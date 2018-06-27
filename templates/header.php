ebcb1a4efb73f4d4d69be4f331e5508c17e5555b
Inao <iona.thomas@centrale.centralelille.fr>
2018-06-27 10:13:34 +0200

    Header avec masquage pour les personnes non connectées

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html >
+<html>
 <head>
     <meta charset="utf-8">
     <title>Todo App</title>
@@ -15,12 +15,14 @@
     <h1 class="app_title"><a href="index.php?view=home">Todo App</a></h1>
     <nav class="menu">
         <ul>
-            <li class="menu_item"><a href="index.php?view=home">Home</a></li>
             <?php
             //Code à insérer pour afficher la liste des pages disponibles
-            $groups=prompt_grp();
-            foreach ($groups as $grp){
-                echo "<li class=\"menu_item\"><a href=\"index.php?view=group&grp_name='$grp[id]'\">$grp[title]</a></li>";
+            if (valider("online", "SESSION")) {
+                echo "<li class=\"menu_item\"><a href=\"index.php?view=home\">Home</a></li>";
+                $groups = prompt_grp();
+                foreach ($groups as $grp) {
+                    echo "<li class=\"menu_item\"><a href=\"index.php?view=group&grp_id=$grp[id]\">$grp[title]</a></li>";
+                }
             }
 
             ?>
@@ -30,10 +32,8 @@
     <div class="account">
         <?php
         // Si l'utilisateur est connecte, on affiche un lien de deconnexion, sinon on affiche un lien de connexion
-
-        echo "<svg class='account_icon' viewBox=\"0 0 48 48\"><path d=\"M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm0 6c3.31 0 6 2.69 6 6 0 3.32-2.69 6-6 6s-6-2.68-6-6c0-3.31 2.69-6 6-6zm0 28.4c-5.01 0-9.41-2.56-12-6.44.05-3.97 8.01-6.16 12-6.16s11.94 2.19 12 6.16c-2.59 3.88-6.99 6.44-12 6.44z\"/></svg>";
-
         if (valider("online", "SESSION")) {
+            echo "<svg class='account_icon' viewBox=\"0 0 48 48\"><path d=\"M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm0 6c3.31 0 6 2.69 6 6 0 3.32-2.69 6-6 6s-6-2.68-6-6c0-3.31 2.69-6 6-6zm0 28.4c-5.01 0-9.41-2.56-12-6.44.05-3.97 8.01-6.16 12-6.16s11.94 2.19 12 6.16c-2.59 3.88-6.99 6.44-12 6.44z\"/></svg>";
             echo "<strong>$_SESSION[usr_login]</strong>";
             echo "<a href=\"controller.php?action=logout\"><button>Log out</button></a>";
         } else {
